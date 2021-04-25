ff2eaa1adc4f8014dcb36cd8dcc43f7ff35b53f4
zyanwei2011 <zyanwei2011@163.com>
2021-04-25 18:26:04 +0800

    config

--- flask-zlkt/config.py
+++ flask-zlkt/config.py
@@ -5,12 +5,18 @@ DEBUG = True
 
 SECRET_KEY = os.urandom(24)
 
-HOSTNAME = '127.0.0.1'
-PORT     = '3306'
-DATABASE = 'zlktqa_demo'
-USERNAME = 'root'
-PASSWORD = 'root'
-DB_URI = 'mysql+mysqldb://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
-SQLALCHEMY_DATABASE_URI = DB_URI
+# 连接数据库字符串
+# dialect+driver://username:password@host:port/database?charset=utf8mb4
+
+dialect = 'mysql'  
+driver = 'pymysql' 
+username = 'root'
+password = '123456'
+host = '47.96.67.53'
+port = 3306
+database = 'zlkt'
+
+
+SQLALCHEMY_DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4'.format(dialect, driver, username, password, host,port, database)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
